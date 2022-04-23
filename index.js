3ab6ba9e52ac80127102ccf65d81d981541395ce
Victor Matos <antoniovitomatos@gmail.com>
2022-04-23 10:51:32 -0300

    gitignore

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ import express from 'express';
 const app = express();
 
 app.post("/sign-up", (req, res) => {
-    res.send('Hello World');
+    
 });
 
 app.get("/", (req, res) => {
-    res.send('Hello World');
+    
 });
 
 app.listen(5000);
